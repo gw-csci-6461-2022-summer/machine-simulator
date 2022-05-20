7cfed1561656901118af5d3d0edf710761d8da4f
zineb.kadhi <zineb.kadhi@everbridge.com>
2022-05-19 23:34:25 -0400

    gui-buttons

---
+++ gui.py
@@ -0,0 +1,27 @@
+from ast import Load
+from mimetypes import init
+from tkinter import *
+
+root = Tk()
+root.configure(background='blue')
+
+def store():
+    myLabel = Label(root, text = "Store")
+    myLabel.pack()
+
+def load():
+    myLabel = Label(root, text = "Load")
+    myLabel.pack()
+
+def init():
+    myLabel = Label(root, text = "Init")
+    myLabel.pack()
+
+storeButton = Button(root, text="store",command=store,fg='blue')
+storeButton.pack()
+laodButton = Button(root, text="Load",command=load,fg='blue')
+laodButton.pack()
+initButton = Button(root, text="Init",command=init,fg='blue')
+initButton.pack()
+
+root.mainloop()
