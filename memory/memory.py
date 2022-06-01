def238acbb231add78f3ec3c2a4beaf392636328
Catherine Meadows <meadowsc@gwu.edu>
2022-06-01 12:06:37 -0400

    memory class, test memory file, and input txt program

---
+++ memory/memory.py
@@ -0,0 +1,28 @@
+# defining class memory 
+class Memory:
+    
+    # defining __init__() function to assign values to object properties 
+    def __init__(self, size):
+        self.size = size # in words  
+        
+        # create mem of size w init values of 0
+        self.mem = {}
+        for i in range(size):
+            self.mem[i] = 0
+
+    # getter for memory size  
+    def get_memory_size(self):
+        return self.size
+
+    # return value at mem location, word addressable 
+    def get_memory_value(self, word):
+        return self.mem[word]
+    
+    # store value at mem location:
+    def store_memory_value(self, word, value):
+        self.mem[word] = value
+
+    # reset memory to 0s
+    def reset_memory(self):
+        for i in range(self.size):
+            self.mem[i] = 0
