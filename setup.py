0762941e052d4ca7506a0447b4e86aef9a45e3af
Joseph Crandall <jwcrand@gmail.com>
2022-06-03 14:07:08 -0400

    tests

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ import os
 import py2exe
 from distutils.core import setup
 
-setup(console=['gui_grid.py'])
-os.rename('dist/gui_grid.exe','dist/machine-simulator.exe')
+setup(console=['machsim/main.py'])
+os.rename('dist/main.exe','dist/machine-simulator.exe')
