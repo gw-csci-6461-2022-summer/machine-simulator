e165a11ac247c1ef0a77937c0576b5f042e16756
DecosDev <55217548+DecosDev@users.noreply.github.com>
2022-06-05 07:50:38 -0600

    added binary return

--- machsim/registers/mar.py
+++ machsim/registers/mar.py
@@ -2,21 +2,24 @@ from pc import pc
 
 # mar holds the address of the next location in memory to be accessed. This value will be obtained from the program counter.
 class mar(pc):
-    def __init_(test_file):
+    def __init_(self,test_file):
         super().__init__(test_file)
 
     def get_pc_value(self):   
         self.increment_pc()
-        if self.value < len(self.hold_value):
+        ram_mem = self.hold_value
+        loaded_value = self.hold_value[self.value]
+        if self.value < len(ram_mem):
             with open("mar_holder.txt", "w") as f:
-                f.write(str(self.hold_value[self.value][1]))
-            return self.hold_value[self.value][1]
+                f.write(str(loaded_value[0]))
+            return loaded_value[0]
         else:
             print("Process Complete.")
 
        
-
-
+    def convert_to_binary(self):
+        vale = self.get_pc_value()
+        return "{0:b}".format(vale)
 
 
 
