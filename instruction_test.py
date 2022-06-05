2315ba984eb98bd178a4af18d7a15a8cf957a5f9
zineb.kadhi <zineb.kadhi@everbridge.com>
2022-06-05 10:48:17 -0400

    new test for load & store + decode is commented

--- instruction_test.py
+++ instruction_test.py
@@ -5,8 +5,14 @@ from Registers.indexRegister import indexRegister
 from Registers.mar import mar
 from instruction import Instruction
 import helper_functions
+from memory import Memory
+from cpu import CPU
 
-inst = Instruction()
+test_memory = Memory(20)
+test_memory.store_memory_value(20, 9348)
+test_cpu = CPU()
+
+inst = Instruction(test_cpu, test_memory)
 inst.instruction_value = '0000011100011111'
 inst.split_instruction()
 opcode = inst.get_opcode()
@@ -21,5 +27,9 @@ print('converted gpr',int(index_gpr, base=2))
 # print('testing decoding:',opcode.decoding_instruction())
 ixr = indexRegister(register_name="IX1", register_size=16, value=2, ixr_number=helper_functions.binary_to_decimal(inst.get_index_ixr()))
 mar_test = mar("mar", 12, 0)
-inst.decoding_instruction()
-inst.load(ixr, mar_test)
+
+# inst.decoding_instruction()
+inst.load()
+inst1 = Instruction(test_cpu, test_memory)
+inst1.instruction_value = '0000101100011101'
+inst1.store()
