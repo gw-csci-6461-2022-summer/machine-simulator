fd34ed840e559d69565a99ecbc4628c0fa3fd9c5
Catherine Meadows <meadowsc@gwu.edu>
2022-07-09 17:44:25 -0400

    New ops (#17)
    * NOT inst
    
    * ORR instruction
    
    * AND inst
    
    * trr inst
    
    * DVD inst
    
    * DVD inst
    
    * MLT inst

--- instruction.py
+++ instruction.py
@@ -24,6 +24,8 @@ class Instruction:
         self.address = []
         self.memory = memory
         self.cpu = cpu
+        self.rx = []
+        self.ry = []
 
     # figuring out the string that corresponds to every instruction attribute 
     def split_instruction(self):
@@ -33,6 +35,22 @@ class Instruction:
         self.indirect_addressing = self.instruction_value[10]
         self.address = self.instruction_value[11:]
         return self.opcode, self.index_gpr,self.index_ixr, self.indirect_addressing, self.address
+    
+    # get rx for opcodes 20 - 25 (reg to reg ops) 
+    def get_rx(self):
+        self.rx = self.instruction_value[6:8]
+        if self.rx == str('00') : return 0 
+        elif self.rx == str('01') : return 1
+        elif self.rx == str('10') : return 2 
+        elif self.rx == str('11') : return 3
+    
+    # get ry for opcodes 20 - 25 (reg to reg ops) 
+    def get_ry(self):
+        self.ry = self.instruction_value[8:10]
+        if self.ry == str('00') : return 0 
+        elif self.ry == str('01') : return 1
+        elif self.ry == str('10') : return 2 
+        elif self.ry == str('11') : return 3
 
     # defining getters to access instruction attributes 
     def get_opcode(self):
@@ -80,6 +98,269 @@ class Instruction:
             print ('Instruction: LDA')
             self.execute_loadA()
             return 3
+        elif self.opcode == 20:
+            print ('Instruction: MLT')
+            self.execute_mlt()
+            return 20
+        elif self.opcode == 21:
+            print ('Instruction: DVD')
+            self.execute_dvd()
+            return 21
+        elif self.opcode == 22:
+            print ('Instruction: TRR')
+            self.execute_trr()
+            return 22
+        elif self.opcode == 23:
+            print ('Instruction: AND')
+            self.execute_and()
+            return 23
+        elif self.opcode == 24:
+            print ('Instruction: ORR')
+            self.execute_orr()
+            return 24
+        elif self.opcode == 25:
+            print ('Instruction: NOT')
+            self.execute_not()
+            return 25
+        
+    def get_r_val(self, gpr_index): 
+        if gpr_index == 0:
+            value = self.cpu.gpr0.get_value()
+        elif gpr_index == 1:
+            value = self.cpu.gpr1.get_value()
+        elif gpr_index == 2:
+            value = self.cpu.gpr2.get_value()
+        else:
+            value = self.cpu.gpr3.get_value()
+        
+        return value
+      
+    # multiply register by register (c(rx) * c(ry))
+    # rx must be 0 or 2; ry must 0 or 2      
+    def execute_mlt(self):
+        # if rx or ry not 0 or 2, don't do instruction 
+        # rx 
+        gpr_index = self.get_rx()
+        print("RX", gpr_index)
+        rx_val = self.get_r_val(gpr_index)
+        if (isinstance(rx_val, str)):
+            rx_val = helper_functions.binary_to_decimal(rx_val)
+            
+        if (gpr_index != 0 and gpr_index != 2): 
+            print("invalid MLT inst")
+            return
+        
+        # ry 
+        gpry_index = self.get_ry()
+        print("RY", gpry_index)
+        ry_val = self.get_r_val(gpry_index)
+        if (isinstance(ry_val, str)):
+            ry_val = helper_functions.binary_to_decimal(ry_val)
+        
+        if (gpry_index != 0 and gpry_index != 2): 
+            print("invalid MLT inst")
+            return
+        
+        print("RX val", rx_val)
+        print("RY val", ry_val)
+        
+        mlt_val = rx_val * ry_val
+        
+        mlt_val = bin(mlt_val)
+       
+        high_order_bits = mlt_val[:len(mlt_val)//2]
+        low_order_bits = mlt_val[len(mlt_val)//2:]
+        print(high_order_bits)
+        print(low_order_bits)
+        
+        high_order_bits = helper_functions.binary_to_decimal(high_order_bits[2:].zfill(16))
+        low_order_bits = helper_functions.binary_to_decimal(low_order_bits.zfill(16))
+        
+        print("high order:", high_order_bits)
+        print("low order:", low_order_bits)
+        
+        if gpr_index == 0:
+            self.cpu.gpr0.set_value(high_order_bits)
+            self.cpu.gpr1.set_value(low_order_bits)
+        elif gpr_index == 2:
+            self.cpu.gpr2.set_value(high_order_bits)
+            self.cpu.gpr3.set_value(low_order_bits)
+        
+        return
+        
+    
+    # rx, rx+1 <- c(rx) / c(ry) 
+    # rx & ry must be either 0 or 2
+    # rx contains quotient; rx+1 contains remainder
+    # if c(ry) = 0, set cc(3) to 1 (set DIVZERO flag)
+    def execute_dvd(self):
+        # rx 
+        gpr_index = self.get_rx()
+        print("RX", gpr_index)
+        rx_val = self.get_r_val(gpr_index)
+        if (isinstance(rx_val, str)):
+            rx_val = helper_functions.binary_to_decimal(rx_val)
+            
+        if (gpr_index != 0 and gpr_index != 2): 
+            print("invalid DVD inst")
+            return
+        
+        # ry 
+        gpry_index = self.get_ry()
+        print("RY", gpry_index)
+        ry_val = self.get_r_val(gpry_index)
+        if (isinstance(ry_val, str)):
+            ry_val = helper_functions.binary_to_decimal(ry_val)
+        
+        if (gpry_index != 0 and gpry_index != 2): 
+            print("invalid DVD inst")
+            return
+        
+        if ry_val == 0:
+            print("Divide by 0!")
+            # TO DO: set cc(3) to 1
+            return
+        
+        # get quotient 
+        q = rx_val // ry_val
+        remainder = rx_val % ry_val
+        
+        print("Quotient", q)
+        print("Remainder", remainder)
+        
+        if gpr_index == 0:
+            self.cpu.gpr0.set_value(q)
+            self.cpu.gpr1.set_value(remainder)
+        elif gpr_index == 2:
+            self.cpu.gpr2.set_value(q)
+            self.cpu.gpr3.set_value(remainder)
+        
+        return
+    
+    # if c(rx) = c(ry), set cc(4) to 1; else set cc(4) to 0
+    def execute_trr(self):
+        # rx 
+        gpr_index = self.get_rx()
+        print("RX", gpr_index)
+        rx_val = self.get_r_val(gpr_index)
+        if (isinstance(rx_val, str)):
+            rx_val = helper_functions.binary_to_decimal(rx_val)
+        
+        # ry 
+        gpry_index = self.get_ry()
+        print("RY", gpry_index)
+        ry_val = self.get_r_val(gpry_index)
+        if (isinstance(ry_val, str)):
+            ry_val = helper_functions.binary_to_decimal(ry_val)
+            
+        if rx_val == ry_val:
+            print("set cc(4) to 1")
+            # TO DO 
+        else:
+            print("set cc(4) to 0")
+            # TO DO
+        return
+    
+    # c(rx) <- c(rx) AND c(ry)
+    def execute_and(self):
+        # rx 
+        gpr_index = self.get_rx()
+        print("RX", gpr_index)
+        rx_val = self.get_r_val(gpr_index)
+        if (isinstance(rx_val, str)):
+            rx_val = helper_functions.binary_to_decimal(rx_val)
+        
+        # ry 
+        gpry_index = self.get_ry()
+        print("RY", gpry_index)
+        ry_val = self.get_r_val(gpry_index)
+        if (isinstance(ry_val, str)):
+            ry_val = helper_functions.binary_to_decimal(ry_val)
+        
+        and_val = rx_val & ry_val
+        print("result of or", and_val)
+        
+        if gpr_index == 0:
+            self.cpu.gpr0.set_value(and_val)
+        elif gpr_index == 1:
+            self.cpu.gpr1.set_value(and_val)
+        elif gpr_index == 2:
+            self.cpu.gpr2.set_value(and_val)
+        else:
+            self.cpu.gpr3.set_value(and_val)
+        
+        return
+    
+    # c(rx) <- c(rx) OR c(ry)
+    def execute_orr(self):
+        # rx 
+        gpr_index = self.get_rx()
+        print("RX", gpr_index)
+        rx_val = self.get_r_val(gpr_index)
+        if (isinstance(rx_val, str)):
+            rx_val = helper_functions.binary_to_decimal(rx_val)
+        
+        # ry 
+        gpry_index = self.get_ry()
+        print("RY", gpry_index)
+        ry_val = self.get_r_val(gpry_index)
+        if (isinstance(ry_val, str)):
+            ry_val = helper_functions.binary_to_decimal(ry_val)
+        
+        or_val = rx_val | ry_val
+        print("result of or", or_val)
+        
+        if gpr_index == 0:
+            self.cpu.gpr0.set_value(or_val)
+        elif gpr_index == 1:
+            self.cpu.gpr1.set_value(or_val)
+        elif gpr_index == 2:
+            self.cpu.gpr2.set_value(or_val)
+        else:
+            self.cpu.gpr3.set_value(or_val)
+        
+        return
+    
+    # c(rx) <-- Logical NOT c(rx)
+    def execute_not(self):
+        # rx 
+        gpr_index = self.get_rx()
+        print("Negating GPR", gpr_index)
+        
+        if gpr_index == 0:
+            rx = self.cpu.gpr0.get_value()
+            if (isinstance(rx, str)):
+                rx = helper_functions.binary_to_decimal(rx)
+            print(rx)
+            rx = rx ^ 0xFFFF
+            print(rx)
+            self.cpu.gpr0.set_value(rx)
+        elif gpr_index == 1:
+            rx = self.cpu.gpr1.get_value()
+            if (isinstance(rx, str)):
+                rx = helper_functions.binary_to_decimal(rx)
+            print(rx)
+            rx = rx ^ 0xFFFF
+            print(rx)
+            self.cpu.gpr1.set_value(rx)
+        elif gpr_index == 2:
+            rx = self.cpu.gpr2.get_value()
+            if (isinstance(rx, str)):
+                rx = helper_functions.binary_to_decimal(rx)
+            print(rx)
+            rx = rx ^ 0xFFFF
+            print(rx)
+            self.cpu.gpr2.set_value(rx)
+        else:
+            rx = self.cpu.gpr3.get_value()
+            if (isinstance(rx, str)):
+                rx = helper_functions.binary_to_decimal(rx)
+            print(rx)
+            rx = rx ^ 0xFFFF
+            print(rx)
+            self.cpu.gpr3.set_value(rx)
+        
+        return
         
     def execute_halt(self):
         # stop running program
